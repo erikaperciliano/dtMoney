42819ba0771373f3a3e73260d580130c51c735c0
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-20 15:45:39 +0000

    feat: setting up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>dtMoney</h1>
-    </>
+    </ThemeProvider>
   )
 }
