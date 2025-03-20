5d3ef4d87e016ed6a652dabd57e4508ca861c83d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-20 17:05:21 +0000

    feat: add Transaction Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionContainer, TransactionsTable } from "./styles";
 
 export function Transactions () {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Website development</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    $12.000,00
+                                </PriceHighlight>    
+                            </td>
+                              
+                            <td>Sale</td>
+                            <td>13/04/2024</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - $59.000
+                                </PriceHighlight>    
+                            </td>
+                            <td>Food</td>
+                            <td>09/04/2024</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionContainer>
         </div>
     )
 }
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -10,7 +10,7 @@ export const defaultTheme = {
     'gray-800': '#202024',
     'gray-900': '#121214',
 
-    'green-300': '##00B37E',
+    'green-300': '#00B37E',
     'green-500': '#00875F',
     'green-700': '#015F43',
 
