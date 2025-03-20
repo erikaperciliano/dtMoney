7de1d3c4474b993031866576691565b7eb37dc91
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-20 19:43:40 +0000

    feat: adds radio button

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup  from "@radix-ui/react-radio-group";
 import styled from "styled-components";
 
 export const Overlay = styled(Dialog.Overlay)`
@@ -68,7 +69,7 @@ export const CloseButton = styled(Dialog.Close)`
     color: ${props => props.theme['gray-500']};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 1rem;
@@ -79,7 +80,7 @@ interface TansactionTypeButtonProps {
     variant: 'income' | 'outcome';
 }
 
-export const TransactionTypeButton = styled.button<TansactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TansactionTypeButtonProps>`
     background: ${props => props.theme['gray-700']};
     padding: 1rem;
     display: flex;
@@ -93,6 +94,15 @@ export const TransactionTypeButton = styled.button<TansactionTypeButtonProps>`
 
     svg {
         color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-300']};
+    }
 
+    &[data-state="unchecked"]:hover {
+        background: ${props => props.theme['gray-600']};
+        transition: background-color 0.2s;
+    }
+
+    &[data-state="checked"] {
+        color: ${props => props.theme['white']};
+        background: ${props => props.variant === 'income' ? props.theme['green-500'] : props.theme['red-500']};
     }
 `;
