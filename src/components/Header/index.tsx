ec626d2993543ab82b63f9e416d129b284bcb8db
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-21 20:28:36 +0000

    fix: updates the import statement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/logo.svg';
+import logoImg from '../../assets/Logo.svg';
 import * as Dialog from "@radix-ui/react-dialog";
 import { NewTransactionModal } from "../NewTransactionModal";
 
