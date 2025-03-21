24eee4b33500c124bc9fccab1ed71ffc8ca2d294
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-03-21 18:53:00 +0000

    feat: searching for transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,11 +10,12 @@ interface Transaction {
 }
 
 
-interface TransactionContext {
+interface TransactionContextType {
     transactions: Transaction[];
+    fetchTransactions: (query?: string) => Promise<void>;
 }
 
-export const TransactionsContext = createContext({} as TransactionContext);
+export const TransactionsContext = createContext({} as TransactionContextType);
 
 interface TransactionsProviderProps {
     children: ReactNode;
@@ -23,19 +24,25 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
        const [transactions, setTransactions] = useState<Transaction[]>([])
     
-        async function loadTransactions() {
-            const response = await fetch('http://localhost:3333/transactions');
+        async function fetchTransactions(query?: string) {
+            const url = new URL('http://localhost:3333/transactions')
+
+            if(query) {
+                url.searchParams.append('q', query)
+            }
+
+            const response = await fetch(url);
             const data = await response.json();
     
             setTransactions(data);
         }
     
         useEffect(() => {
-            loadTransactions()
+            fetchTransactions()
         }, []);
         
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
             {children}
         </TransactionsContext.Provider>
     )
